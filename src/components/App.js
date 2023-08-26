3053289bb19f314853c3c180f1e480b27cfb2e08
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2023-08-26 15:15:26 +0300

    add loginPage

--- src/components/App.js
+++ src/components/App.js
@@ -3,36 +3,37 @@ import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from './Layout/Layout'
+import { LoginPage } from './pages/loginPage/LoginPage';
 
 const HomePage = lazy(() => import('./pages/HomePage'))
 
 function App() {
   return (
 
-  <Routes>
-    <Route path='/' element={<Layout />}>
-      <Route index element={<HomePage />}/>
-      
-      <Route />
-      <Route />
-      
-    </Route>
-    
-    <Route path='/login' element={
-				<Suspense>
-					{/* <PublicRoute>
-						<LoginPage />
-					</PublicRoute> */}
-				</Suspense>
-		}/>
+	<Routes>
+		<Route path='/' element={<Layout />}>
+		<Route index element={<HomePage />}/>
+		
+		<Route />
+		<Route />
+		
+		</Route>
+		
+		<Route path='/login' element={
+			<Suspense>
+						 {/* <PublicRoute> */}
+							<LoginPage />
+						{/* </PublicRoute>  */}
+			</Suspense>
+			}/>
 		<Route path='/signUp' element={
 			<Suspense>
-				{/* <PublicRoute>
-					<RegistrationPage />
-				</PublicRoute> */}
+					{/* <PublicRoute>
+						<RegistrationPage />
+					</PublicRoute> */}
 			</Suspense>
-		}/>
-  </Routes>
+			}/>
+	</Routes>
   )
   
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,57 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 
 import style from './style.module.css'
+import { LoginPage } from "../pages/loginPage/LoginPage"
 
 const Header = () => {
+
+    const navigate = useNavigate()
+	const handleLogin = () => {
+		navigate('/login')
+	}
+
 	return (
 		<>
-            <nav className={style.nav}>
-                <div>
-                    <NavLink to='/'>Home</NavLink>
+            <nav className={style.navbar}>
+                <div class={style.logo}>ANDREW</div>  {/* <!-- LOGO --> */}
 
-                    <NavLink to='/contacts'>Contacts</NavLink>
+                <ul className={style.nav__links}>  {/* <!-- NAVIGATION MENU --> */}
+                    
+                    {/* <!-- USING CHECKBOX HACK --> */}
+                    <input type="checkbox" id="checkbox_toggle" />
+                    <label for="checkbox_toggle" className={style.hamburger}>&#9776;</label>
+                    
+                    <div className={style.menu}>  {/* <!-- NAVIGATION MENUS --> */}
+                        <li>
+                            <NavLink to='/'>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/contacts'>Contacts</NavLink>
+                        </li>
+                        <li className={style.services}>
+                            <a href="/">Services</a>
 
-                    {/* <NavLink to='/contacts'>Contacts</NavLink> */}
-                </div>
+                            {/* <!-- DROPDOWN MENU --> */}
+                            <ul className={style.dropdown}>
+                                <li><a href="/">Dropdown 1 </a></li>
+                                <li><a href="/">Dropdown 2</a></li>
+                                <li><a href="/">Dropdown 2</a></li>
+                                <li><a href="/">Dropdown 3</a></li>
+                                <li><a href="/">Dropdown 4</a></li>
+                            </ul>
+                        </li>
+                    </div>
+                </ul>
+                         
                 <div>
-                    {/* <button
-		// 				className='btn btn-outline-success'
-		// 				onClick={profile ? handleLogOut : handleLogin}
-		// 			>
-		// 				{profile ? 'LogOut' : 'LogIn'}
-		// 			</button> */}
+                    <button
+                        className={style.btn}
+                        onClick={handleLogin}
+						// onClick={profile ? handleLogOut : handleLogin}
+                    >
+                        Login
+						{/* {profile ? 'LogOut' : 'LogIn'} */}
+					</button> 
                 </div>
             </nav>
 		</>
